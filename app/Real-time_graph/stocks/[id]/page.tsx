53d32f73fab80e49c1938a81009d5bda1cae0519
krisapat <pluemkrisapat@gmail.com>
2025-05-03 01:52:52 +0700

    อัปเดตโค้ดใหม่

--- app/Real-time_graph/stocks/[id]/page.tsx
+++ app/Real-time_graph/stocks/[id]/page.tsx
@@ -5,29 +5,43 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import { Undo2 } from 'lucide-react'
 import { quicksand } from '@/utils/font'
-export default async function StockDetail({ params }: { params: { id: string } }) {
-  const stock = stockSymbols.find(s => s.id.toLowerCase() === params.id.toLowerCase())
 
-  if (!stock) return <div className="p-4">ไม่พบข้อมูลหุ้น</div>
+export default async function StockDetail({
+  params,
+}: {
+  params: Promise<{ id: string }>
+}) {
+  const { id } = await params;
+  const stock = stockSymbols.find(
+    (s) => s.id.toLowerCase() === id.toLowerCase()
+  );
+
+  if (!stock) return <div className="p-4">ไม่พบข้อมูลหุ้น</div>;
 
   return (
     <div className="h-screen pt-20 px-4">
       <div className="flex space-x-4 my-4">
-        <h1 className={`${quicksand.className} text-2xl font-bold`}>{stock.id}</h1>
-        <Button className='bg-white dark:bg-black/30'>
-          <Link href={'/Real-time_graph'}>
-            <Undo2 className='text-black dark:text-white' />
+        <h1 className={`${quicksand.className} text-2xl font-bold`}>
+          {stock.id}
+        </h1>
+        <Button className="bg-white dark:bg-black/30">
+          <Link href={"/Real-time_graph"}>
+            <Undo2 className="text-black dark:text-white" />
           </Link>
         </Button>
       </div>
       <div className="grid grid-cols-1 lg:grid-cols-4 gap-6">
         <div className="lg:col-span-3">
-          <AdvancedChart symbol={stock.symbol} uniqueId={stock.id + '-detail'} />
+          <AdvancedChart
+            symbol={stock.symbol}
+            uniqueId={stock.id + "-detail"}
+          />
         </div>
         <div className="lg:col-span-1">
           <StockNews symbol={stock.id} />
         </div>
       </div>
     </div>
-  )
+  );
 }
+
