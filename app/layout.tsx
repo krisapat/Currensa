8abfb8e5bd85470bf55c45750b24a3dc7bb9e90d
krisapat <pluemkrisapat@gmail.com>
2025-05-03 15:24:46 +0700

    อัปเดตโค้ดใหม่

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/navbar/navbarcom/darkmode/theme-provider";
 import Navbar from "@/components/navbar/Navbar";
