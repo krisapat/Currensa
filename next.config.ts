5a8c996bb6f98d90273e175514c093a5ef2ad1be
krisapat <pluemkrisapat@gmail.com>
2025-05-03 13:22:04 +0700

    อัปเดตโค้ดใหม่

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  i18n: {
-    locales: ['en', 'th', 'jp'],
-    defaultLocale: 'en',
-  },
-  reactStrictMode: true,
-
 };
 
 export default nextConfig;
